307d0c7e500874c1969e3038cfc6e50e707d0d96
Danilo Dias <danilolive30@hotmail.com>
2024-08-27 01:38:00 -0300

    feat: collumns

---
+++ src/components/Columns/index.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const Columns = () => {
+  return (
+    <div className="container-collumns">
+      <p>Personagem</p>
+      <p>Séries</p>
+      <p>Eventos</p>
+    </div>
+  );
+};
+
+export default Columns;
--- src/modules/Characters/index.tsx
+++ src/modules/Characters/index.tsx
@@ -1,18 +1,29 @@
+import Columns from "../../components/Columns";
 import { TextField } from "../../components/Input";
 import "./index.css";
 
 const Characters = () => {
+  //  ON CHANGE AQUI FILTRANDO OS PERSONAGENS COM DEBOUNCE
+
   return (
     <div className="container-characters">
       <p className="title">Busca de personagens</p>
-
-      <div style={{
-        maxWidth: 295
-      }}> 
-        <TextField placeholder="Search" label="Nome do personagem"/>
+      <div
+        style={{
+          maxWidth: 295,
+        }}
+      >
+        <TextField
+          endAdornment={<img src="magnifier-icon.png" />}
+          placeholder="Search"
+          label="Nome do personagem"
+          // OnChange
+        />
       </div>
 
-      <div>{/* COLUNAS, PODE UTILIZAR GRD */}</div>
+      <div>
+        <Columns />
+        {/* COLUNAS, PODE UTILIZAR GRD */}</div>
 
       <div>{/* CARD */}</div>
     </div>
